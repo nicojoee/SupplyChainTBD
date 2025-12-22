917daf5db791fd89bdf1fb0c39bf78441824bda5
Nicholas Joe Sumantri <nicholas.joe.sumantri@gmail.com>
2025-12-22 12:06:40 +0700

    Update for Vercel deployment (removed secrets)

--- api/index.php
+++ api/index.php
@@ -1,21 +1,3 @@
 <?php
 
-// Set the working directory to the project root
-chdir(__DIR__ . '/..');
-
-// Load the Composer autoloader
-require __DIR__ . '/../vendor/autoload.php';
-
-// Bootstrap Laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Handle the incoming request
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
