236aa4c1b08995db4d696a9c323d425b7853aecd
Nicholas Joe Sumantri <nicholas.joe.sumantri@gmail.com>
2025-12-22 13:09:15 +0700

    Add bundled CA cert for TiDB SSL on Vercel

--- config/database.php
+++ config/database.php
@@ -58,8 +58,8 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') && env('MYSQL_ATTR_SSL_CA') ? [
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            'options' => extension_loaded('pdo_mysql') && (env('MYSQL_ATTR_SSL_CA') || env('APP_ENV') === 'production') ? [
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', base_path('storage/certs/ca-cert.pem')),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false),
             ] : [],
         ],
